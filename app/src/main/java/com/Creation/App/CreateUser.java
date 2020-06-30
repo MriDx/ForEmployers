e9f7b49815ce9052d9079e2aed1d01b1abeb6b7a
appcreationp <67642775+appcreationp@users.noreply.github.com>
2020-06-30 19:58:39 +0530

    i

---
+++ app/src/main/java/com/Creation/App/CreateUser.java
@@ -0,0 +1,146 @@
+package com.Creation.App;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CreateUser extends AppCompatActivity {
+    EditText mfullName,mEmail,mPassword,mPhone,mcompanyName;
+    Button mRegisterBtn;
+    Button malreadRegisteredBtn;
+    FirebaseAuth fAuth;
+    ProgressBar progressBar;
+    FirebaseFirestore fStore;
+    String userId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_user);
+
+        mfullName = findViewById(R.id.fullName);
+        mEmail = findViewById(R.id.textEmailId);
+        mcompanyName = findViewById(R.id.companyName);
+        mPassword = findViewById(R.id.editPassword);
+        mPhone = findViewById(R.id.editPhone);
+        mRegisterBtn = findViewById(R.id.RegisterBtn);
+        malreadRegisteredBtn = findViewById(R.id.alreadyRegisteredBtn);
+
+        fAuth = FirebaseAuth.getInstance();
+        progressBar = findViewById(R.id.progressBar);
+        fStore = FirebaseFirestore.getInstance();
+
+
+
+        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+                final String phone = mPhone.getText().toString();
+                final String fullName = mfullName.getText().toString();
+                final String companyName = mcompanyName.getText().toString();
+
+                if(TextUtils.isEmpty(email)){
+                    mEmail.setError("Email is Required");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    mPassword.setError("Password is Required");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(phone)){
+                    mPhone.setError("Phone Number is Required");
+                    return;
+                }
+                if(phone.length() >= 11){
+                    mPhone.setError("Phone Number should be equal to 10 character");
+                    return;
+                }
+
+                if(password.length() < 6){
+                    mPassword.setError("Password must be more than 6 character");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(companyName)){
+                    mcompanyName.setError("Company Name is mandatory");
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+                // register the user in Firebase
+
+                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(CreateUser.this, "User Created", Toast.LENGTH_SHORT).show();
+                            userId = fAuth.getCurrentUser().getUid();
+                            DocumentReference documentReference = fStore.collection("Users Detail").document(userId);
+                            Map<String,Object> user = new HashMap<>();
+                            user.put ("Full Name",fullName);
+                            user.put ("Phone Number",phone);
+                            user.put ("Email Id",email);
+                            user.put ("Company Name",companyName);
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d("TAG", "onSuccess: user Profile is Created for"+ userId);
+                                }
+                            });
+                            startActivity(new Intent(getApplicationContext(),Login.class));
+
+
+                        }else {
+                            Toast.makeText(CreateUser.this, "Error !"+ task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+
+
+            }
+        });
+
+
+
+
+
+
+
+
+
+        Button alreadyRegisteredBtn = findViewById(R.id.alreadyRegisteredBtn);
+        alreadyRegisteredBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/Creation/App/ProfileTab.java
@@ -0,0 +1,54 @@
+package com.Creation.App;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+public class ProfileTab extends AppCompatActivity {
+    TextView fullName,phone,email,companyName;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String UserId;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_tab);
+        fullName = findViewById(R.id.profileName);
+        phone = findViewById(R.id.profilePhone);
+        email =findViewById(R.id.profileEmail);
+        companyName = findViewById(R.id.profileCompanyName);
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        UserId = fAuth.getCurrentUser().getUid();
+
+        DocumentReference documentReference = fStore.collection("Users Detail").document(UserId);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+           fullName.setText(documentSnapshot.getString("Full Name"));
+           phone.setText(documentSnapshot.getString("Phone Number"));
+           email.setText(documentSnapshot.getString("Email Id"));
+           companyName.setText(documentSnapshot.getString("Company Name"));
+
+            }
+        });
+
+
+
+    }
+
+
+}
