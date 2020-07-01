9d6972fb6ca2436dadcbef76a6baf3f9d6de7853
Anand Raj <raj050300@gmail.com>
2020-07-01 16:23:03 +0530

    Rebasing

--- app/src/main/java/com/Creation/App/CreateUser.java
+++ app/src/main/java/com/Creation/App/CreateUser.java
@@ -1,8 +1,5 @@
 package com.Creation.App;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -13,13 +10,14 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -28,9 +26,9 @@ import java.util.Map;
 import java.util.Objects;
 
 public class CreateUser extends AppCompatActivity {
-    EditText mfullName,mEmail,mPassword,mPhone,mcompanyName;
+    EditText mfullName, mEmail, mPassword, mPhone, mcompanyName;
     Button mRegisterBtn;
-    Button malreadRegisteredBtn;
+   
     FirebaseAuth fAuth;
     ProgressBar progressBar;
     FirebaseFirestore fStore;
@@ -55,7 +53,6 @@ public class CreateUser extends AppCompatActivity {
         fStore = FirebaseFirestore.getInstance();
 
 
-
         mRegisterBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -65,31 +62,31 @@ public class CreateUser extends AppCompatActivity {
                 final String fullName = mfullName.getText().toString();
                 final String companyName = mcompanyName.getText().toString();
 
-                if(TextUtils.isEmpty(email)){
+                if (TextUtils.isEmpty(email)) {
                     mEmail.setError("Email is Required");
                     return;
                 }
 
-                if(TextUtils.isEmpty(password)){
+                if (TextUtils.isEmpty(password)) {
                     mPassword.setError("Password is Required");
                     return;
                 }
 
-                if(TextUtils.isEmpty(phone)){
+                if (TextUtils.isEmpty(phone)) {
                     mPhone.setError("Phone Number is Required");
                     return;
                 }
-                if(phone.length() >= 11){
+                if (phone.length() >= 11) {
                     mPhone.setError("Phone Number should be equal to 10 character");
                     return;
                 }
 
-                if(password.length() < 6){
+                if (password.length() < 6) {
                     mPassword.setError("Password must be more than 6 character");
                     return;
                 }
 
-                if(TextUtils.isEmpty(companyName)){
+                if (TextUtils.isEmpty(companyName)) {
                     mcompanyName.setError("Company Name is mandatory");
                     return;
                 }
@@ -97,47 +94,29 @@ public class CreateUser extends AppCompatActivity {
                 progressBar.setVisibility(View.VISIBLE);
                 // register the user in Firebase
 
-                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                fAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
-                            // send verification link
-
-                            FirebaseUser fuser = fAuth.getCurrentUser();
-                            fuser.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
-                                @Override
-                                public void onSuccess(Void aVoid) {
-                                    Toast.makeText(CreateUser.this, "Verification Link is Send to your Email", Toast.LENGTH_SHORT).show();
-                                }
-                            }).addOnFailureListener(new OnFailureListener() {
-                                @Override
-                                public void onFailure(@NonNull Exception e) {
-                                    Log.d("TAG", "onFailure: Email not sent" + e.getMessage());
-                                }
-                            });
-
-
-
-
+                        if (task.isSuccessful()) {
                             Toast.makeText(CreateUser.this, "User Created", Toast.LENGTH_SHORT).show();
                             userId = Objects.requireNonNull(fAuth.getCurrentUser()).getUid();
                             DocumentReference documentReference = fStore.collection("Users Detail").document(userId);
-                            Map<String,Object> user = new HashMap<>();
-                            user.put ("Full Name",fullName);
-                            user.put ("Phone Number",phone);
-                            user.put ("Email Id",email);
-                            user.put ("Company Name",companyName);
+                            Map<String, Object> user = new HashMap<>();
+                            user.put("Full Name", fullName);
+                            user.put("Phone Number", phone);
+                            user.put("Email Id", email);
+                            user.put("Company Name", companyName);
                             documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
                                 @Override
                                 public void onSuccess(Void aVoid) {
-                                    Log.d("TAG", "onSuccess: user Profile is Created for"+ userId);
+                                    Log.d("TAG", "onSuccess: user Profile is Created for" + userId);
                                 }
                             });
-                            startActivity(new Intent(getApplicationContext(),Login.class));
+                            startActivity(new Intent(getApplicationContext(), Login.class));
 
 
-                        }else {
-                            Toast.makeText(CreateUser.this, "Error !"+ Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(CreateUser.this, "Error !" + Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
                             progressBar.setVisibility(View.GONE);
                         }
                     }
