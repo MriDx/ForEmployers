03f9c4d63f20499fbb276d50f82009a30b18b49e
appcreationp <67642775+appcreationp@users.noreply.github.com>
2020-07-05 19:22:36 +0530

    All files are upto date.

--- app/src/main/java/com/Creation/App/Employees.java
+++ app/src/main/java/com/Creation/App/Employees.java
@@ -1,18 +1,58 @@
 package com.Creation.App;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+@SuppressWarnings("ConstantConditions")
 public class Employees extends AppCompatActivity {
+
+    FirebaseFirestore fStore;
+    FirebaseAuth fAuth;
+    String userId;
+    ListView listView;
+    private List<String> namesList= new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employees);
+        fStore = FirebaseFirestore.getInstance();
+        fAuth = FirebaseAuth.getInstance();
+        userId = fAuth.getCurrentUser().getUid();
+
+
+
+//All the Buttons. which intregated in bottom.
         Button Monthly=findViewById(R.id.btn_monthly);
         Button Annual=findViewById(R.id.btn_annual);
         Button Employee=findViewById(R.id.btn_employees);
@@ -47,6 +87,83 @@ public class Employees extends AppCompatActivity {
                 Intent intent=new Intent(getApplicationContext(),Profile.class);
                 startActivity(intent);
                 finish();
+            }
+        });
+
+        fStore.collection("User Details").document(userId).collection("Employee List")
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent( QuerySnapshot queryDocumentSnapshots, FirebaseFirestoreException e) {
+                        namesList.clear();
+                        for (DocumentSnapshot documentSnapshot : queryDocumentSnapshots){
+                            namesList.add(documentSnapshot.getString("Full Name"));
+                        }
+                        ArrayAdapter<String>adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.activity_list_item, namesList);
+                        adapter.notifyDataSetChanged();
+                        listView.setAdapter(adapter);
+                    }
+                });
+
+
+
+
+
+
+
+
+        //Add
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                LayoutInflater factory = LayoutInflater.from(Employees.this);
+
+//text_entry is an Layout XML file containing two text field to display in alert dialog
+                final View textEntryView = factory.inflate(R.layout.text_entry, null);
+
+                final EditText input1 = (EditText) textEntryView.findViewById(R.id.EditText1);
+                final EditText input2 = (EditText) textEntryView.findViewById(R.id.EditText2);
+
+
+                input1.setText(" ", TextView.BufferType.EDITABLE);
+                input2.setText(" ", TextView.BufferType.EDITABLE);
+
+                final AlertDialog.Builder alert = new AlertDialog.Builder(Employees.this);
+                alert.setView(textEntryView).setPositiveButton("Save",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,
+                                                int whichButton) {
+                                final String name=input1.getText().toString();
+
+                                String rf_id=input2.getText().toString();
+
+                                userId = Objects.requireNonNull(fAuth.getCurrentUser()).getUid();
+                                DocumentReference documentReference = fStore.collection("Users Detail").document(userId).collection("Employee List").document(rf_id);
+                                Map<String, Object> user = new HashMap<>();
+                                user.put("Full Name", name);
+
+                                documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        Log.d("TAG", "onSuccess: user Profile is Created for" + userId);
+                                        Toast.makeText(getApplicationContext(), "Id Created for"+name, Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+
+                                /* User clicked OK so do some stuff */
+
+                            }
+                        }).setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,
+                                                int whichButton) {
+                                /*
+                                 * User clicked cancel so do some stuff
+                                 */
+                            }
+                        });
+                alert.show();
             }
         });
 
