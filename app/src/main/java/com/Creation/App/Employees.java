03bc8c6c6d3d9981ab06547b4256fa9c6c593c05
Anand Raj <raj050300@gmail.com>
2020-07-06 00:29:37 +0530

    anand

--- app/src/main/java/com/Creation/App/Employees.java
+++ app/src/main/java/com/Creation/App/Employees.java
@@ -1,9 +1,5 @@
 package com.Creation.App;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,9 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -22,7 +23,6 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -38,41 +38,41 @@ public class Employees extends AppCompatActivity {
 
     FirebaseFirestore fStore;
     String userId;
-    TextView get_user;
+
+    List<String> rf_id, name;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employees);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+
         fAuth = FirebaseAuth.getInstance();
 
         fStore = FirebaseFirestore.getInstance();
-        Button Monthly=findViewById(R.id.btn_monthly);
-        Button Annual=findViewById(R.id.btn_annual);
-        Button Employee=findViewById(R.id.btn_employees);
-        Button Profile=findViewById(R.id.btn_profile);
-        get_user=findViewById(R.id.get_user);
+        Button Monthly = findViewById(R.id.btn_monthly);
+        Button Annual = findViewById(R.id.btn_annual);
+        Button Employee = findViewById(R.id.btn_employees);
+        Button Profile = findViewById(R.id.btn_profile);
+
         userId = Objects.requireNonNull(fAuth.getCurrentUser()).getUid();
-        FirebaseFirestore.getInstance().collection("Users Detail/"+userId+"/Employee List").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                if (task.isSuccessful()) {
-                    List<String> list = new ArrayList<>();
-                    for (QueryDocumentSnapshot document : Objects.requireNonNull(task.getResult())) {
-                        list.add(document.getString("Full Name"));
-                    }
-                    Log.d("TAG", list.toString());
-                    get_user.setText(list.toString());
-                    Toast.makeText(Employees.this, list.toString(), Toast.LENGTH_SHORT).show();
-                } else {
-                    Log.d("TAG", "Error getting documents: ", task.getException());
-                    Toast.makeText(Employees.this, "Error getting documents", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+
+        work();
+
+
         Monthly.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(getApplicationContext(),Monthly.class);
+                Intent intent = new Intent(getApplicationContext(), Monthly.class);
                 startActivity(intent);
                 finish();
             }
@@ -80,7 +80,7 @@ public class Employees extends AppCompatActivity {
         Annual.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(getApplicationContext(),Annual.class);
+                Intent intent = new Intent(getApplicationContext(), Annual.class);
                 startActivity(intent);
                 finish();
             }
@@ -88,7 +88,7 @@ public class Employees extends AppCompatActivity {
         Employee.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(getApplicationContext(),Employees.class);
+                Intent intent = new Intent(getApplicationContext(), Employees.class);
                 startActivity(intent);
                 finish();
             }
@@ -96,7 +96,7 @@ public class Employees extends AppCompatActivity {
         Profile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(getApplicationContext(),Profile.class);
+                Intent intent = new Intent(getApplicationContext(), Profile.class);
                 startActivity(intent);
                 finish();
             }
@@ -125,9 +125,9 @@ public class Employees extends AppCompatActivity {
                         new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog,
                                                 int whichButton) {
-                                final String name=input1.getText().toString();
+                                final String name = input1.getText().toString();
 
-                                String rf_id=input2.getText().toString();
+                                String rf_id = input2.getText().toString();
 
                                 userId = Objects.requireNonNull(fAuth.getCurrentUser()).getUid();
                                 DocumentReference documentReference = fStore.collection("Users Detail").document(userId).collection("Employee List").document(rf_id);
@@ -138,7 +138,8 @@ public class Employees extends AppCompatActivity {
                                     @Override
                                     public void onSuccess(Void aVoid) {
                                         Log.d("TAG", "onSuccess: user Profile is Created for" + userId);
-                                        Toast.makeText(getApplicationContext(), "Id Created for"+name, Toast.LENGTH_SHORT).show();
+                                        Toast.makeText(getApplicationContext(), "Id Created for" + name, Toast.LENGTH_SHORT).show();
+                               work();
                                     }
                                 });
 
@@ -158,5 +159,42 @@ public class Employees extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void work() {
+
+        FirebaseFirestore.getInstance().collection("Users Detail/" + userId + "/Employee List").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    rf_id = new ArrayList<>();
+                    name = new ArrayList<>();
+                    for (QueryDocumentSnapshot document : Objects.requireNonNull(task.getResult())) {
+                        name.add(document.getString("Full Name"));
+                        rf_id.add(document.getId());
+                    }
+
+                    mAdapter = new MyRecyclerViewAdapter(getDataSet());
+                    mRecyclerView.setAdapter(mAdapter);
+                    Log.d("TAG", rf_id.toString());
+
+                  //  Toast.makeText(Employees.this, rf_id.toString(), Toast.LENGTH_SHORT).show();
+                } else {
+                    Log.d("TAG", "Error getting documents: ", task.getException());
+                    Toast.makeText(Employees.this, "Error getting documents", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private ArrayList<DataObject> getDataSet() {
+        ArrayList results = new ArrayList<DataObject>();
+        DataObject obja = new DataObject("Name", "Rf id", "other", "other");
+        results.add(0, obja);
+        for (int index = 0; index < rf_id.size(); index++) {
+            DataObject obj = new DataObject(name.get(index), rf_id.get(index), " ", " ");
+            results.add(index + 1, obj);
+        }
+        return results;
     }
 }
