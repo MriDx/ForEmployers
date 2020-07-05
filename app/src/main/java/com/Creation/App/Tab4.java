912d869c4f128a3198a8445185d98cc054e1ca62
appcreationp <67642775+appcreationp@users.noreply.github.com>
2020-07-05 08:50:09 +0530

    All files are upto date.

--- app/src/main/java/com/Creation/App/Tab4.java
+++ app/src/main/java/com/Creation/App/Tab4.java
@@ -1,17 +1,37 @@
 package com.Creation.App;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.auth.User;
+
+import java.util.concurrent.Executor;
 
 /**
  * Created by Belal on 2/3/2016.
  */
 
 //Our class extending fragment
+@SuppressWarnings({"ConstantConditions", "FinalizeCalledExplicitly"})
 public class Tab4 extends Fragment {
+    TextView fullName,phone,email,companyName;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String UserId;
 
     //Overriden method onCreateView
     @Override
@@ -19,6 +39,19 @@ public class Tab4 extends Fragment {
 
         //Returning the layout file after inflating
         //Change R.layout.tab1 in you classes
-        return inflater.inflate(R.layout.tab4, container, false);
+        View view = inflater.inflate(R.layout.tab4, container, false);
+        fullName = view.findViewById(R.id.profileName);
+        phone = view.findViewById(R.id.profilePhone);
+        email = view.findViewById(R.id.profileEmail);
+        companyName = view.findViewById(R.id.profileCompanyName);
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        UserId = fAuth.getCurrentUser().getUid();
+
+
+
+        return view;
     }
 }
