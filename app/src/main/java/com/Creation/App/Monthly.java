80556c4517f77f822cf8aa67457a2db716da3243
appcreationp <67642775+appcreationp@users.noreply.github.com>
2020-07-25 16:40:28 +0530

    Spinner (Toasted the Value of the Spinner)

--- app/src/main/java/com/Creation/App/Monthly.java
+++ app/src/main/java/com/Creation/App/Monthly.java
@@ -65,7 +65,6 @@ public class Monthly extends AppCompatActivity {
 
                     Toast.makeText(Monthly.this, "Seleceted the Desgination " + item, Toast.LENGTH_SHORT).show();
 
-
                 }
 
             }
